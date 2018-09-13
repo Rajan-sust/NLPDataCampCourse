e2d4fbe495b4bc103492db24210dc93b1d7b01da
Rajan Saha Raju <imrajon0@gmail.com>
2018-09-13 17:38:11 +0600

    Update ChoiceResponse.py

--- Chapter1/ChoiceResponse.py
+++ Chapter1/ChoiceResponse.py
@@ -1,15 +1,15 @@
 import random
 
-
 responses = {
-    "what's your name?" : [
-        "my name is EchoBot",
-        "they call me EchoBot",
+    "what's your name?" : ["my name is EchoBot","they call me EchoBot",
         "the name's Bot, Echo Bot"
     ]
 }
 
+default = ["sorry!I'd not understand you","Soory! I'm small brain"]
+
 def respond(msg) :
     if msg in responses :
         return random.choice(responses[msg])
+    return random.choice(default)
 
